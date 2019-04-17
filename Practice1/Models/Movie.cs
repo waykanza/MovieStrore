a68709721ef284897e6ecedb6f02720770697775
waykanza <apriant.wahyu@gmail.com>
2019-04-18 01:20:33 +0700

    Add project files.

---
+++ Practice1/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Practice1.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleasDate { get; set; }
+        public string Genre { get; set; }
+        public Decimal Price { get; set; }
+
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
